89051ed7b87f9bd2f2aaf1fd7f51767d0ec1ff27
dqh19930227 <ding_qinghui@163.com>
2022-06-10 19:19:42 +0800

    no message

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dingqinghui/gopack/sum
+
+go 1.18
---
+++ sum/sum.go
@@ -0,0 +1,13 @@
+/**
+ * @Author: dingQingHui
+ * @Description:
+ * @File: sum
+ * @Version: 1.0.0
+ * @Date: 2022/6/10 19:17
+ */
+
+package sum
+
+func Sum(a, b int) int {
+	return a + b
+}
