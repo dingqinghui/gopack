c8c3edd4abc1fdd837ad7dcd5286edb0bee80dc3
dqh19930227 <ding_qinghui@163.com>
2022-06-13 10:44:38 +0800

    no message

---
+++ api.go
@@ -0,0 +1,15 @@
+/**
+ * @Author: dingQingHui
+ * @Description:
+ * @File: api
+ * @Version: 1.0.0
+ * @Date: 2022/6/13 10:43
+ */
+
+package gopack
+
+import "github.com/dingqinghui/gopack/sum"
+
+func Sum(a, b int) int {
+	return sum.Sum(a, b)
+}
